e6333f9e96993de56d24f0e52d0f1f1618befcf1
PCS <shubhlodaliya@gmail.com>
2026-05-26 17:59:16 +0530

    fix: import useReveal hook in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { CursorGlow } from "@/components/CursorGlow";
 import { AiPhotoshoot } from "@/components/AiPhotoshoot";
+import { useReveal } from "@/hooks/use-reveal";
 import { Route, Routes } from "react-router-dom";
 
 function HomePage() {
